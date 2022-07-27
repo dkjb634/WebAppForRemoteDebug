f6f34a33f104d3e9508dea30c26d02c29416c04e
dkjb634 <dmitry.kazantsev@jetbrains.com>
2022-07-27 19:53:44 +0300

    InitialCommit

---
+++ WebAppForRemoteDebug/Views/Home/MyPage.cshtml.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebAppForRemoteDebug.Views
+{
+    public class MyPage : PageModel
+    {
+        public void OnGet()
+        {
+            Console.WriteLine("qweqwewq");
+        }
+    }
+}
